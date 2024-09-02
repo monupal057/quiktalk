54ccd9ad1ffeb6b1673f6b4b969d9bacbca052fc
Monu Pal <monupal057@gmail.com>
2024-09-03 01:47:32 +0530

    backend api fixed part 4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 		port: 3000,
 		proxy: {
 			"/api": {
-				target: "https://quiktalk-4.onrender.com",
+				target: "https://quiktalkvibess.onrender.com",
 				// target: "http://localhost:8080",
 			},
 		},
